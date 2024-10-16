24fe03aba69dc2bd0cb5685cde75d0b4b8e697e4
nguyenvietdiepk66hust <diep1hai3@gmail.com>
2024-10-16 16:52:19 +0700

    BLINK

--- src/main.cpp
+++ src/main.cpp
@@ -1,19 +1,17 @@
 #include <Arduino.h>
-
 // put function declarations here:
-int myFunction(int, int);
 
 void setup() {
+  Serial.begin(9600);
+  pinMode(LED_BUILTIN,OUTPUT);
   // put your setup code here, to run once:
-  int result = myFunction(2, 3);
+
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
-  Serial.print("hello Platform IO");
-  myFunction();
+ digitalWrite(LED_BUILTIN,HIGH);
+ delay(1000);
+ digitalWrite(LED_BUILTIN,LOW);
+ delay(1000);
 
-// put function definitions here:
-int myFunction(int x, int y) {
-  return x + y;
 }
